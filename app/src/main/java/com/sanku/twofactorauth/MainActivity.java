788e6869e1c4a2337505d3fbdb589c55cab792b8
Your Name <you@example.com>
2019-01-29 15:57:55 -0500

    simple test

--- app/src/main/java/com/sanku/twofactorauth/MainActivity.java
+++ app/src/main/java/com/sanku/twofactorauth/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // this is test
          mAuth = FirebaseAuth.getInstance();
 
         etUserPhonenumber = (EditText) findViewById(R.id.etUserPhonenumber);
